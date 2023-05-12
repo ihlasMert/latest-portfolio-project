fc30c50d394afefa35f0d08258528445db9cb0cf
ihlasMert <ihlas1mert@gmail.com>
2023-05-12 17:59:50 +0300

    added o project commit

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -3,6 +3,7 @@ import Navbar from "../coponents/Navbar";
 import Footer from "../coponents/Footer";
 import HeroImg2 from "../coponents/HeroImg2";
 import PricingCard from "../coponents/PricingCard";
+import WorkCard from "../coponents/WorkCard";
 
 const Project = () => {
   return (
@@ -10,9 +11,7 @@ const Project = () => {
       <Navbar />
       <HeroImg2 heading="PROJECTS." text="Some of my most recent works" />
       <PricingCard />
-      <section>
-        <Footer></Footer>
-      </section>
+      <WorkCard />
     </div>
   );
 };
