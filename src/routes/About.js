a195f97663c7b8df9f4fd3bf1d5e3b53520ffaaa
ihlasMert <ihlas1mert@gmail.com>
2023-05-12 17:01:08 +0300

    Added about section

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../coponents/Navbar";
 import Footer from "../coponents/Footer";
+import HeroImg2 from "../coponents/HeroImg2";
 
 const About = () => {
   return (
     <div>
       <Navbar />
+      <HeroImg2 />
       <Footer />
     </div>
   );
